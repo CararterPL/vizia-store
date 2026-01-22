5f3cb6e227204cff54d1fcafe7e36c83d0059b5c
CaracterPL <kacper.nowek@o2.pl>
2026-01-22 11:21:08 +0100

    Remove unused blog routes

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,13 +1,19 @@
 'use client';
 
-import React from 'react';
+import React, { use } from 'react';
 import { Header } from "../../../components/layout/Header";
 import { Footer } from "../../../components/layout/Footer";
 import { ProductGallery } from "../../../components/product/ProductGallery";
 import { ProductInfo } from "../../../components/product/ProductInfo";
 import { CrossSelling } from "../../../components/product/CrossSelling";
 
-export default function ProductPage() {
+// Definiujemy, że funkcja przyjmuje params (czyli to [id] z folderu)
+export default function ProductPage({ params }: { params: Promise<{ id: string }> }) {
+  
+  // W nowym Next.js (który wykrył Vercel) params trzeba "rozpakować" przez use()
+  const resolvedParams = use(params);
+  const id = resolvedParams.id;
+
   return (
     <div className="bg-vizia-black min-h-screen flex flex-col">
       <Header />
@@ -17,22 +23,23 @@ export default function ProductPage() {
           {/* Główny grid produktu */}
           <div className="grid lg:grid-cols-2 gap-12 xl:gap-24 items-start">
               
-            {/* LEWA STRONA: Galeria (Zawsze pierwsza na mobile) */}
+            {/* LEWA STRONA: Galeria */}
             <div className="w-full order-1 lg:order-1">
               <ProductGallery />
             </div>
 
-            {/* PRAWA STRONA: Info (Druga na mobile, sticky na desktopie) */}
+            {/* PRAWA STRONA: Info */}
             <div className="w-full order-2 lg:order-2 lg:sticky lg:top-32">
               <ProductInfo />
             </div>
 
           </div>
 
-          {/* SEKACJA POD PRODUKTEM: Cross Selling (Klocek 3) */}
+          {/* SEKACJA POD PRODUKTEM: Cross Selling */}
           <div className="mt-32 border-t border-white/5 pt-24">
             <h4 className="mb-12">Uzupełnij garaż</h4>
-            <CrossSelling />
+            {/* Teraz id jest zdefiniowane powyżej, więc komponent je otrzyma */}
+            <CrossSelling currentProductId={id} />
           </div>
         </div>
       </main>
